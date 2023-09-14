b6676176895905f7e59f727d979df2cc12e1ae51
Muhammed Said Gülberk <62105391+MSaidG@users.noreply.github.com>
2023-09-14 14:23:14 +0300

    Finish all the exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
+const add = function(num1, num2) {
 	
+  return num1 + num2;
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
 	
+  return num1 - num2;
 };
 
-const sum = function() {
+const sum = function(arr) {
 	
+  let sum = 0;
+  arr.forEach(element => {
+
+    sum += element;
+  });
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
 
+  let multiply = 1;
+  arr.forEach(element => {
+
+    multiply *= element;
+  });
+
+  return multiply;
 };
 
-const power = function() {
+const power = function(num1, num2) {
 	
+  return num1 ** num2;
 };
 
-const factorial = function() {
+const factorial = function(num) {
 	
+  let result = 1;
+
+  if (num === 0 || num === 1) return 1;
+
+  for (num; num > 1; num--)
+  {
+    result *= num;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if (typeof num === "string") +num;
+
+    if (num < 0) return "OOPS";
+
+    if (num === 1 || num === 2) return 1;
+
+    let oldResult = 1;
+    let result = 1;
+    let temp = 0
+    for (let i = 2; i < num; i++)
+    {
+        temp = result;
+        result += oldResult; 
+        oldResult = temp;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
